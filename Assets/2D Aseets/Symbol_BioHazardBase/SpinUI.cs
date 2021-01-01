7ad489524c7f895b1c685a0451d6071a75b8028e
ikeda_yuichiro_110 <ikeda.yuichiro.w.00@gmail.com>
2021-01-01 23:03:48 +0900

    オプション画面/アバター制作画面のカメラ制御

---
+++ Assets/2D Aseets/Symbol_BioHazardBase/SpinUI.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SpinUI : MonoBehaviour
+{
+    public float Speed;
+    RectTransform rect;
+    void Start() => rect = GetComponent<RectTransform>();
+    void Update() => rect.Rotate(Vector3.forward * Speed * Time.deltaTime);
+}
---
+++ Assets/_MakeAvator/MakeAvatorScene_ctrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeAvatorScene_ctrl : MonoBehaviour
+{
+    //CG CAMERA -------------------------------------------------
+    [Space(20)]
+    [SerializeField] Transform _CameraRoot;
+    [SerializeField] Transform _Camera;
+    [SerializeField] Transform _ModelRoot;
+    [SerializeField] float VerticalMove;
+    [SerializeField] float Radius;
+    [SerializeField] float RadiusMin;
+    [SerializeField] float SpinSpeed;
+    float v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CG CAMERA ------------------------------------------------
+        v += Time.deltaTime;
+        _CameraRoot.position = _ModelRoot.position + Vector3.up * Mathf.Sin(v);
+        _Camera.position = _CameraRoot.position;
+        _Camera.LookAt(_CameraRoot);
+        _Camera.Translate(Vector3.back *(RadiusMin + Mathf.Sin(v) * Radius),Space.Self);
+        _CameraRoot.Rotate(Vector3.up * SpinSpeed * Time.deltaTime, Space.World);
+        //UI System------------------------------------------------------
+
+        //UserModel Update ---------------------------------------------------
+    }
+}
