4f2e006bbc175eebc24a0ecc28e313ab84dcaec8
園田 <e184223sk@kurume-it.ac.jp>
2021-01-19 14:33:28 +0900

    NetworkSuper の追加（オンラインの基本的な機能をまとめたクラス）

---
+++ Assets/sonoda/Script/ForbiddenWords.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//不適切な表現を規制するクラス
+public class ForbiddenWords
+{
+    #region Static Fields
+
+    //禁止ワード一覧　あまり開かないことをおすすめします
+    static readonly string[] Words = new string[]{
+"青姦","あおかん",
+"アメ公","あめこう",
+"アルコール依存症",
+"犬殺し","いぬごろし",
+"淫売","いんばい","売春",
+"うんこ","うんこ",
+"うんち",
+"穢多","えた",
+"ガキ","がき",
+"皮被り","かわかぶり","包茎",
+"姦通","かんつう",
+"キ印","きじるし","精神障害者",
+"キチ","きち",
+"気違い",
+"屑屋","くず","クズ",
+"くわえ込む","くわえこむ",
+"クンニ","くんに",
+"強姦","ごうかん",
+"ゴミ","ごみ",
+"千摺り","せんずり","オナニー",
+"ちんこ","チンコ","ちんぽ","チンポ","ちんちん","チンチン","ソープ",
+"非人","ひにん",
+"ブス","ぶす",
+"部落","ぶらく",
+"マンコ","まんこ","女性器","ほーみ","べちょこ","おめこ",
+"セックス","せっくす"
+,"あなる","アナル",
+"おっぱい","オッパイ",
+"死ね",
+"ガイジ","がいじ",
+"エロ","インポ","いんぽ","陰毛","淫乱","えっち","エッチ","おしり","顔射","がんしゃ","射精","スケベ","スカトロ","絶倫","前立腺","ちぇりーぼーい","チェリーボーイ","童貞","どうてい","てまん","手マン","なかだし","膣","発情","フェラ","ホモ","ほも","マリファナ","レイプ","性交",
+"シコシコ",
+"fuck","Fuck",
+"shit","Shit",
+"Suck","suck",
+"faggot","Faggot",
+"Nigger","Negro","Nigga","nigger","negro","nigga",
+"sex","Sex","SEX","piss","dick","Dick","cunt","Cunt","tits","Tits",
+"ass","Ass","prick","1919","4545","0721"
+
+    };
+
+ 
+    /// <summary>
+    /// 禁止ワードが含まれているか判定　含まれていたらtrue　ない場合はfalse
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static bool WordFilter(string text)
+    {
+        foreach (string s in Words)
+        {
+            if (text.Contains(s))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    #endregion
+}
+
+
+
---
+++ Assets/sonoda/Script/NetworkSuper.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonobitEngine;
+using System;
+public class NetworkSuper :MonobitEngine.MonoBehaviour
+{
+
+    //オンラインの最低限な機能をまとめたクラス  　機能：入室、退室、チャット
+
+    //抽象クラスではないが継承して使うことを推奨します
+
+    //チャットはこのままでは使えないので子クラスでメソッドを拡張してください
+
+
+
+    #region Static Fields
+
+    MonobitView _view;
+
+    #endregion
+
+
+
+
+    #region Protected Methods
+
+    #region Chat 
+
+    [MunRPC]
+    protected virtual void ReceiveChat(string message)
+    {
+
+    }
+
+
+    public virtual void SendChat(string text)
+    {
+        if (String.IsNullOrEmpty(text))
+            return;
+
+        //不適切な表現を削除--------------------------------------------------------------
+
+        text = text.Replace(" ", "").Replace("　", "").Trim();
+        if (ForbiddenWords.WordFilter(text))
+        {
+            text += Environment.NewLine + "<color=red>不適切な表現を含むため送信できません</color>";
+            return;
+        }
+
+        //---------------------------------------------------------------------------------
+
+
+        //送信
+        _view.RPC("ReceiveChat", MonobitTargets.All, text);
+    }
+
+
+    #endregion
+
+
+
+        /// <summary>
+        /// roomが存在していればそこに入る。ない場合は新しく作る
+        /// </summary>
+        /// <param name="room"></param>
+    protected virtual void EnterRoom(RoomData room)
+    {
+        RoomData[] Rooms = MonobitNetwork.GetRoomData();
+        if (!MonobitNetwork.inLobby)
+        {
+            MonobitNetwork.JoinLobby();
+            if (Contain(Rooms, room))
+            {
+                MonobitNetwork.JoinRoom(room.name);
+            }
+            else
+            {
+                MonobitNetwork.CreateRoom(room.name);
+            }
+        }
+
+    }
+
+    protected virtual void LeaveRoom()
+    {
+        if(MonobitNetwork.inRoom)   MonobitNetwork.LeaveLobby();
+    }
+
+    #endregion
+
+
+    #region Private Methods
+
+    /// <summary>
+    /// RoomDataの配列の中に任意のRoomDataが入っているか
+    /// </summary>
+    /// <param name="Rooms">RoomData配列</param>
+    /// <param name="room">調べるRoomData</param>
+    /// <returns></returns>
+    private bool Contain(RoomData[] Rooms, RoomData room)
+    {
+        for(int i = 0;i < Rooms.Length; i++)
+        {
+            if(Rooms[i] ==room)
+            {
+                return true;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return false;
+
+
+    }
+
+
+    #endregion
+
+
+
+    #region Unity Callbacks
+    private void Start()
+    {
+        if(_view == null)
+        {
+            _view = this.gameObject.AddComponent<MonobitView>();
+        }
+    }
+
+
+    #endregion
+}
