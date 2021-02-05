3bc0b14f8bd3f3dad2e01847f6aaa2b73e5180f1
e194228th <e194228th@kurume-it.ac.jp>
2021-02-05 20:22:28 +0900

    プログラムとアニメーションコントローラです

---
+++ Assets/Takeuchi/FallDamage.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FallDamage : MonoBehaviour
+{
+
+    public GameObject HP_object = null;    //使うTextオブジェクト
+    public int HP_num = 1000;   //HPの量（初期値100）
+    private int Damage = 0; //ダメージの量
+    public int PlayerY = 0; //ジャンプした時点でのY座標
+    public int nowPlayerY = 0;  //現在のY座標
+    public int DamageM = 1; //ダメージ倍率
+    public bool NoDamage = false;
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //nowPlayerYに現在のY座標を入れ続ける
+        Vector3 Player = GameObject.Find("unitychan").transform.position;
+        nowPlayerY = (int)Player.y;
+        //Unity上で指定したTextオブジェクトからTextコンポーネントを取得
+        Text HP_text = HP_object.GetComponent<Text>();
+
+        //テキストに現在のHPを表示させる
+        HP_text.text = "HP:" + HP_num;
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")   //ジャンプした時
+        {
+            //ジャンプ時点のY座標をPlayerYに保存
+            Vector3 Player = GameObject.Find("unitychan").transform.position;
+            PlayerY = (int)Player.y;
+            //Debug.Log("ジャンプ");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground") //着地した時
+        {
+            animator.SetBool("Ground_Exit", true);
+            //落下ダメージがあるか判定
+            if (NoDamage == false)
+            {
+                FDamageJud();
+            }
+            //ダメージ分HPを減らす
+            HP_num -= Damage;
+            Debug.Log(Damage+"のダメージを受けた");  //判定の確認用（消して大丈夫です）
+            //ダメージを初期値に戻す
+            Damage = 0;
+        }
+        animator.SetBool("Ground_Exit", false);
+    }
+
+    void FDamageJud()   //落下ダメージの判定
+    {
+        int Jud = PlayerY-nowPlayerY;
+        if (Jud > 2)    //ジャンプした時点のY座標と現在のY座標の差が"2"以上のときダメージが入る（""内は全体を見て調整）
+        {
+            Damage = Jud/2*DamageM;
+        }
+    }
+}
---
+++ Assets/Takeuchi/PlayerControl.cs
@@ -0,0 +1,159 @@
+﻿//using MonobitEngine;//動かすときはコメントアウト消す
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour //動かすときはMonobitEngine.をMonoBehaviourの前につける
+{
+    Rigidbody rb;
+    public float defaultSpeed = 5f;
+    public float speed = 5f;//移動速度
+    public float jumpForce = 100f;//ジャンプ力
+    public float climbForce = 160f;
+    private bool Ground = true;
+    private float angle = 0;
+    private GameObject PL;
+    private CapsuleCollider playercharacter;
+    private Animator animator;
+
+    void Start()
+    {
+        PL = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        playercharacter= PL.GetComponent<CapsuleCollider>();
+
+    }
+    void Update()
+    {
+        Move();
+        Jump();
+        Climb();
+
+        if (Input.GetMouseButton(1))
+        {
+            rb.AddForce(transform.up * climbForce);
+            transform.position += transform.forward * speed * Time.deltaTime * 10;
+        }
+
+
+    }
+    public void Move()
+    {
+        //Cでダッシュ---------------------------------------------------------------------
+        if (Input.GetKey(KeyCode.C))
+        {
+            speed = defaultSpeed * 2.0f;
+            animator.SetBool("Key_C", true);
+        }
+        else
+        {
+            speed = defaultSpeed;
+            animator.SetBool("Key_C", false);
+        }
+
+        //Wで前進----------------------------------------------------------------------------
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            animator.SetBool("Key_W", true);
+        }
+        else
+        {
+            animator.SetBool("Key_W", false);
+        }
+
+        //Sで後退---------------------------------------------------------------------------
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+            animator.SetBool("Key_S", true);
+        }
+        else
+        {
+            animator.SetBool("Key_S", false);
+        }
+
+        //Aで左へ---------------------------------------------------------------------------
+        if (Input.GetKey(KeyCode.A))
+        {
+            angle -= 2;
+            transform.position += transform.forward * speed / 2 * Time.deltaTime;
+            transform.Rotate(new Vector3(0, -90 * Time.deltaTime, 0));
+            animator.SetBool("Key_A", true);
+        }
+        else
+        {
+            animator.SetBool("Key_A", false);
+        }
+
+        //Dで右へ-----------------------------------------------------------------------------
+        if (Input.GetKey(KeyCode.D))
+        {
+            angle += 2;
+            transform.position += transform.forward * speed / 2 * Time.deltaTime;
+            transform.Rotate(new Vector3(0, 90 * Time.deltaTime, 0));
+            animator.SetBool("Key_D", true);
+        }
+        else
+        {
+            animator.SetBool("Key_D", false);
+        }
+        //--------------------------------------------------------------------------------------
+
+        //Xでしゃがむ
+        if (Input.GetKey(KeyCode.X))
+        {
+            animator.SetBool("Key_X", true);
+            playercharacter.height = 0.775f;
+            //playercharacter.center(0, 0.076, -0.56);
+        }
+        else
+        {
+            animator.SetBool("Key_X", false);
+            playercharacter.height = 1.55f;
+        }
+        //
+    }
+
+    /// <summary>
+    /// ジャンプ
+    /// </summary>
+    public void Jump()
+    {
+        if (Ground == true)
+        {
+            if (Input.GetKey(KeyCode.Space))//Spaceでジャンプ
+            {
+                rb.AddForce(transform.up * jumpForce);
+                animator.SetBool("Key_Space", true);
+                Ground = false;
+            }
+        }
+        if (!Input.GetKey(KeyCode.Space))
+        {
+                animator.SetBool("Key_Space", false);
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            if (Ground == false)
+            {
+                Ground = true;
+            }
+        }
+    }
+    ///　崖上り
+    public void Climb()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(10000000000000000000);
+    }
+  }
