eef756b25e2fcd40eb01fecde04fd632e0d5b8cd
ikeda_ <ikeda.yuichiro.w.00@gmail.com>
2021-03-01 14:25:56 +0900

    chat2

--- Assets/Chat/ChatSync.cs
+++ Assets/Chat/ChatSync.cs
@@ -6,11 +6,16 @@ public class ChatSync : MonoBehaviour
 {
     public static void SEND(ChatUSER user, string text)
     {
+        string str0 = user.ToString();
+        string str1 = text;
         //各PCのADD関数を引数をそのまま入れて呼ぶ
     }
 
-    public static void RECV(ChatUSER user, string text)
+    public static void RECV(string str0, string str1)
     {
+        ChatUSER user = new ChatUSER(str0);
+        string text = str1;
+
         //各PCの
     }
 }
--- Assets/Chat/ChatUSER.cs
+++ Assets/Chat/ChatUSER.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ChatUSER 
 {
@@ -9,5 +10,39 @@ public class ChatUSER
     {
         this.NAME = NAME;
         this.COLOR = COLOR;
+    }
+
+    public ChatUSER(string raw)
+    {
+        string[] r = raw.Split( new string[] { ":.:" }, System.StringSplitOptions.None);
+        try
+        {
+            if (r.Length == 4)
+            {
+                NAME = r[0];
+                COLOR = new Color
+                    (
+                        float.Parse(r[1]),
+                        float.Parse(r[2]),
+                        float.Parse(r[3]),
+                        float.Parse(r[4])
+                    );
+            }
+        }
+        catch (Exception)
+        {
+            Debug.LogError("CHATDATA");
+
+        }
+    }
+
+    public override string ToString()
+    {
+        return
+            NAME + ":.:" +
+            COLOR.r.ToString() + ":.:" +
+            COLOR.g.ToString() + ":.:" +
+            COLOR.b.ToString() + ":.:" +
+            COLOR.a.ToString() + ":.:";
     }
 }
