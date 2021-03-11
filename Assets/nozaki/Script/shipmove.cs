2c160ae73c97ee954cc8c0d6267c9fea42e76cbd
e194142ny <e194142ny@kurume-it.ac.jp>
2021-03-11 20:58:49 +0900

    船

--- Assets/nozaki/Script/shipmove.cs
+++ Assets/nozaki/Script/shipmove.cs
@@ -2,28 +2,38 @@
 
 public class shipmove : MonoBehaviour
 {
-    public float speed = 1;
-    public float torque = 5;
-    public float heightAdjusted = 15;
+    public float speed = 30;
     private float AngleY;
-    public float addspeed = 5;
+    public float posy;
+    public float turnspeed;
 
     // Update is called once per frame
     void Update()
     {
         //操作
-        if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(speed * Vector3.forward * Time.deltaTime, Space.Self) ;
-        if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(speed * Vector3.back *Time.deltaTime, Space.Self);
-        if (Input.GetKey(KeyCode.LeftArrow)) AngleY -= torque * Time.deltaTime;
-        if (Input.GetKey(KeyCode.RightArrow)) AngleY += torque * Time.deltaTime;
-
-        //加速
-        if (Input.GetKey(KeyCode.Space)) speed += addspeed;
-
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(speed * Vector3.forward * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(speed * Vector3.back * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            AngleY -= turnspeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            AngleY += turnspeed * Time.deltaTime;
+        }
         //位置
-        transform.position = new Vector3(transform.position.x, Wave.GetSurfaceHeight(transform.position) + heightAdjusted, transform.position.z);
+        transform.position = new Vector3(transform.position.x, Wave.GetSurfaceHeight(transform.position) + posy, transform.position.z);
 
         //角度
-        transform.rotation = Wave.GetSurfaceNormal(transform.position) * Quaternion.Euler(0, AngleY, 0);
+        Quaternion rot = Wave.GetSurfaceNormal(transform.position);
+        rot = new Quaternion(rot.x, AngleY, rot.z, rot.w);
+        transform.rotation = rot;
+
     }
 }
