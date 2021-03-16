a5d2e36be6dc0d668eea2b4c251bbcbe503f470c
ikeda_ <ikeda.yuichiro.w.00@gmail.com>
2021-03-16 18:47:20 +0900

    部屋番データ

---
+++ Assets/Script/RoomNumber.cs
@@ -0,0 +1,18 @@
+﻿public  static class RoomNumber
+{
+    /// <summary>
+    /// 部屋番
+    /// </summary>
+    public static int NUMBER
+    {
+        get => number;
+        set => number = value > 63 ? 63 : value < 0 ? 0 : value;
+    }
+
+    /// <summary>
+    /// ゲーム終了後　部屋番をリセット
+    /// </summary>
+    public static void ResetNumber() => number = -1;
+
+    static int number;
+}
