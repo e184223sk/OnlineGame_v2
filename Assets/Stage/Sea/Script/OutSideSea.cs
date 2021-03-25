567f5e31600b7d091ab2320e18a3803689223b15
ikeda_ <ikeda.yuichiro.w.00@gmail.com>
2021-03-25 19:35:40 +0900

    bakaoki

--- Assets/Stage/Sea/Script/OutSideSea.cs
+++ Assets/Stage/Sea/Script/OutSideSea.cs
@@ -4,34 +4,18 @@ public class OutSideSea : MonoBehaviour
 {
     public Transform VectorCtrlPointer;
     public Transform Sea0, Sea1, Sea2, Sea3, Sea4, Sea5, Sea6, Sea7;
-
+    public float Distance;
 #if UNITY_EDITOR
-    void OnDrawGizmos()
+    void OnValidate()
     {
-        if (UnityEditor.EditorApplication.isPlaying)
-            return;
-        Start();
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(VectorCtrlPointer.position, 5);
-        Gizmos.DrawLine(VectorCtrlPointer.up * 10, -VectorCtrlPointer.up * 10);
-        Gizmos.DrawLine(VectorCtrlPointer.right * 10, -VectorCtrlPointer.right * 10);
-        Gizmos.DrawLine(VectorCtrlPointer.forward * 10, -VectorCtrlPointer.forward * 10); 
-
-        Gizmos.DrawLine(VectorCtrlPointer.position, Sea0.position);
-        Gizmos.DrawLine(VectorCtrlPointer.position, Sea1.position);
-        Gizmos.DrawLine(VectorCtrlPointer.position, Sea2.position);
-        Gizmos.DrawLine(VectorCtrlPointer.position, Sea3.position);
-        Gizmos.DrawLine(VectorCtrlPointer.position, Sea4.position);
-        Gizmos.DrawLine(VectorCtrlPointer.position, Sea5.position);
-        Gizmos.DrawLine(VectorCtrlPointer.position, Sea6.position);
-        Gizmos.DrawLine(VectorCtrlPointer.position, Sea7.position); 
+        if (!UnityEditor.EditorApplication.isPlaying) Start();  
     }
 #endif
 
     void Start()
     {
         Vector3 p = Wave.wave.transform.position + Vector3.up * 0.1f;
-        Vector3 c = Vector3.one * Wave.wave.GetScale;
+        Vector3 c = Vector3.one * Wave.wave.GetScale* Distance;
         Sea0.position = new Vector3(-c.x, p.y, -c.z);
         Sea1.position = new Vector3(-c.x, p.y,  p.z);
         Sea2.position = new Vector3(-c.x, p.y,  c.z);
