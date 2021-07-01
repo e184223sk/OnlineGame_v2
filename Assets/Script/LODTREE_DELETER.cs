0610b0c8ac416f19bb302be253d21533c7f02acf
ikeda_ <ikeda.yuichiro.w.00@gmail.com>
2021-07-01 13:48:56 +0900

    西村ステージ

---
+++ Assets/Script/LODTREE_DELETER.cs
@@ -0,0 +1,4 @@
+﻿public class LODTREE_DELETER : UnityEngine.MonoBehaviour
+{
+    void Start() => Destroy(gameObject);
+}
---
+++ Assets/SetLOD_Trees.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLOD_Trees : MonoBehaviour
+{
+    [ContextMenu("Method")]
+    private void Method()
+    {
+        var H = transform.Find("High");
+        var M = transform.Find("Mid");
+        var L = transform.Find("Low");
+        if (H.GetComponent<LOD_komono>() == null) H.gameObject.AddComponent<LOD_komono>();
+        if (M.GetComponent<LODTREE_DELETER>() == null) M.gameObject.AddComponent<LODTREE_DELETER>();
+        if (L.GetComponent<LODTREE_DELETER>() == null) L.gameObject.AddComponent<LODTREE_DELETER>();
+    }
+}
